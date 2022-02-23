d42aefe5d047c4a3b58075c984f287f1c9ec0203
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-02-23 15:08:25 +0500

    login

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/Home/Home';
 import UserList from './pages/usersList/UserList';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <>
       <Router>
+        <Route path='/login' exact component={Login} />
         <Topbar />
         <div className='container'>
           <Sidebar />
           <Switch>
             <Route path='/' exact component={Home} />
+
             <Route path='/users' exact component={UserList} />
           </Switch>
         </div>
---
+++ admin-panel/src/pages/Login/Login.jsx
@@ -0,0 +1,210 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Image from './bg.jpg';
+import { deepOrange } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: `url(${Image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'dark'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 6),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+
+    backgroundColor: deepOrange[500],
+    width: 86,
+    height: 86,
+    marginTop: '30px',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  textField: {
+    width: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingBottom: 0,
+    marginTop: '50px',
+    fontWeight: 500,
+  },
+  input: {
+    color: 'white',
+    backgroundColor: '#362245',
+    height: 80,
+    fontSize: '25px',
+  },
+  button: {
+    width: '70%',
+    height: 60,
+    marginTop: '40px',
+    fontSize: '20px',
+    marginLeft: '120px',
+  },
+  checkBox: {
+    color: 'white',
+  },
+  Link: {
+    color: 'white',
+    fontSize: '18px',
+    marginTop: '40px',
+  },
+  SignText: {
+    color: 'white',
+    fontSize: '42px',
+    marginTop: '5px',
+  },
+
+  floatingLabelFocusStyle: {
+    color: 'white',
+    fontSize: '20px',
+  },
+
+  orange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+}));
+
+const Login = (props) => {
+  const classes = useStyles();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  return (
+    <Grid container component='main' className={classes.root}>
+      <Grid item md={7} className={classes.image} />
+      <Grid item style={{ backgroundColor: '#180c2b' }} md={5}>
+        <div style={{ color: '#474745' }} className={classes.paper}>
+          <Avatar
+            sm={{ width: 86, height: 86 }}
+            className={classes.avatar}
+            spacing={4}
+          ></Avatar>
+          <Typography className={classes.SignText} component='h1' variant='h5'>
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              className={classes.textField}
+              InputLabelProps={{ className: classes.floatingLabelFocusStyle }}
+              InputProps={{
+                className: classes.input,
+              }}
+              variant='filled'
+              margin='normal'
+              color='secondary'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              name='email'
+              autoComplete='email'
+              autoFocus
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <br />
+            <TextField
+              className={classes.textField}
+              variant='filled'
+              margin='normal'
+              InputLabelProps={{ className: classes.floatingLabelFocusStyle }}
+              InputProps={{
+                className: classes.input,
+              }}
+              required
+              fullWidth
+              //backgroundColor="#fcfaf7"
+              color='secondary'
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Grid
+              container
+              justify='flex-end'
+              spacing={4}
+              style={{ padding: 20 }}
+            >
+              <Grid item>
+                <Link
+                  className={classes.Link}
+                  href='/forgetpassword'
+                  variant='body2'
+                >
+                  {'Forget Password'}
+                </Link>
+              </Grid>
+            </Grid>
+
+            <Button
+              justify='space-around'
+              className={classes.button}
+              fullWidth
+              variant='contained'
+              color='default'
+              onClick={(e) => {
+                // userService
+                //   .login(email, password)
+                //   .then((data) => {
+                //     console.log(data);
+                //     window.location.href = "/";
+                //   })
+                //   .catch((err) => {
+                //     return alert("Password or Email incorrect");
+                //   });
+              }}
+            >
+              Login
+            </Button>
+
+            <Grid
+              container
+              justify='space-around'
+              spacing={4}
+              style={{ padding: 20 }}
+            >
+              <Grid item>
+                <Link className={classes.Link} href='/register' variant='body2'>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
