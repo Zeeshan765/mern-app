c88e05076630bf90a9c05e9721da0d4772c595a4
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-03-24 11:36:16 +0500

    ok

--- api/routes/ProductRoute.js
+++ api/routes/ProductRoute.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const authorization = require('../middleware/authorization');
 const admin = require('../middleware/admin');
 
-router.post('/product', authorization, admin, async (req, res) => {
+router.post('/product', async (req, res) => {
   res.json('Hey This is Zeeshan');
 });
 
-router.get('/product/new', admin, async (req, res) => {
-  res.json('Here we go');
+router.get('/product/:id', admin, async (req, res) => {
+  res.send('dkd');
 });
 
 module.exports = router;
